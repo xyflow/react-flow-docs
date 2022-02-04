955e66797cf943380cea824b8e5f2af6060294b1
Christopher Möller <chrtze@gmail.com>
2022-02-04 19:08:12 +0100

    add stats section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import CodeBlock from '@theme/CodeBlock';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { Box, Heading, Flex } from '@chakra-ui/react';
+import { Box, Heading, Flex, Text, Button } from '@chakra-ui/react';
 
 import HeroFlow from '../components/HeroFlow';
 import ExampleFlow from '../components/ExampleFlow';
@@ -23,31 +23,43 @@ export default function Home() {
       </Box>
 
       <Box bgColor="bgDark">
-        <Box maxWidth="container.md" mx="auto" px={3} py={100}>
-          <Heading
-            mb={5}
-            fontFamily="body"
-            fontWeight="black"
-            textAlign="center"
-            size="2xl"
-            color="white"
-          >
-            Getting Started
-          </Heading>
-          <Heading
-            fontFamily="body"
-            fontWeight="bold"
-            size="md"
-            mx="auto"
-            textAlign="center"
-            color="gray.300"
-          >
-            React Flow enables you to build node-based applications. From simple static diagrams to
-            complex interactive editors. To get started, you can install it via npm.
-          </Heading>
-          <Box mt={10}>
-            <CodeBlock language="bash">npm install react-flow-renderer</CodeBlock>
-          </Box>
+        <Box maxWidth="container.xl" mx="auto" px={3} py={3}>
+          <Flex color="white" mt={10}>
+            <Flex flex={1 / 2}>
+              <Box flex={1 / 4}>
+                <Text fontSize={12} fontWeight="bold" color="gray.300" textTransform="uppercase">
+                  Github Stars
+                </Text>
+                <Text lineHeight="1" fontWeight="black" fontSize={40}>
+                  8.2K
+                </Text>
+              </Box>
+              <Box flex={1 / 4} mx={3} px={3}>
+                <Text fontSize={12} fontWeight="bold" color="gray.300" textTransform="uppercase">
+                  Last Commit
+                </Text>
+                <Text lineHeight="1" fontWeight="black" fontSize={40}>
+                  2h ago
+                </Text>
+              </Box>
+              <Box flex={1 / 4} mx={3} px={3}>
+                <Text fontSize={12} fontWeight="bold" color="gray.300" textTransform="uppercase">
+                  Discord Members
+                </Text>
+                <Text lineHeight="1" fontWeight="black" fontSize={40}>
+                  324
+                </Text>
+              </Box>
+            </Flex>
+
+            <Box flex={1 / 2}>
+              <Text>
+                We are dedicated to maintaining a high quality library and implementing new features
+                on a regular basis. You can help us by subscribing to one of our Pro plans.
+              </Text>
+              <Button variant="primary">React Flow Pro</Button>
+            </Box>
+          </Flex>
         </Box>
       </Box>
 
