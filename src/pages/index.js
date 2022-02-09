fc32815d84f468096fe1a6e6a1467ba9e0d732e3
Christopher Möller <chrtze@gmail.com>
2022-02-09 14:32:01 +0100

    add showcase section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import CodeBlock from '@theme/CodeBlock';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { Box, Heading, Flex, Text, Button } from '@chakra-ui/react';
+import { Box, Heading, Flex, Text, Button, Image } from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 import HeroFlow from '../components/HeroFlow';
 import ExampleFlow from '../components/ExampleFlow';
@@ -23,49 +24,65 @@ export default function Home() {
       </Box>
 
       <Box bgColor="bgDark">
-        <Box maxWidth="container.xl" mx="auto" px={3} py={3}>
-          <Flex color="white" mt={10}>
-            <Flex flex={1 / 2}>
-              <Box flex={1 / 4}>
-                <Text fontSize={12} fontWeight="bold" color="gray.300" textTransform="uppercase">
+        <Box maxWidth="container.xl" mx="auto" px={3} py={8}>
+          <Flex flexWrap="wrap" color="white" flexDirection={['column', null, null, 'row']}>
+            <Flex alignItems="center" flex={1}>
+              <Box flex={1}>
+                <Text
+                  mb={2}
+                  fontSize={12}
+                  fontWeight="bold"
+                  color="gray.300"
+                  textTransform="uppercase"
+                >
                   Github Stars
                 </Text>
-                <Text lineHeight="1" fontWeight="black" fontSize={40}>
+                <Text mb={0} lineHeight="1" fontWeight="black" fontSize={40}>
                   8.2K
                 </Text>
               </Box>
-              <Box flex={1 / 4} mx={3} px={3}>
-                <Text fontSize={12} fontWeight="bold" color="gray.300" textTransform="uppercase">
+              <Box flex={1} mx={3} px={3}>
+                <Text
+                  mb={2}
+                  fontSize={12}
+                  fontWeight="bold"
+                  color="gray.300"
+                  textTransform="uppercase"
+                >
                   Last Commit
                 </Text>
-                <Text lineHeight="1" fontWeight="black" fontSize={40}>
+                <Text mb={0} lineHeight="1" fontWeight="black" fontSize={40}>
                   2h ago
                 </Text>
               </Box>
-              <Box flex={1 / 4} mx={3} px={3}>
-                <Text fontSize={12} fontWeight="bold" color="gray.300" textTransform="uppercase">
+              <Box flex={1} mx={3} px={3}>
+                <Text
+                  mb={2}
+                  fontSize={12}
+                  fontWeight="bold"
+                  color="gray.300"
+                  textTransform="uppercase"
+                >
                   Discord Members
                 </Text>
-                <Text lineHeight="1" fontWeight="black" fontSize={40}>
+                <Text mb={0} lineHeight="1" fontWeight="black" fontSize={40}>
                   324
                 </Text>
               </Box>
             </Flex>
 
-            <Box flex={1 / 2}>
-              <Text>
-                We are dedicated to maintaining a high quality library and implementing new features
-                on a regular basis. You can help us by subscribing to one of our Pro plans.
+            <Box flex={1}>
+              <Text fontWeight="bold">
+                React Flow is a MIT-licensed open source library. You can help us to ensure the
+                further development and maintanance by subscribing to one of our pro plans.
               </Text>
-              <Button variant="primary">React Flow Pro</Button>
+              <Button variant="link" as="a" href="/pro" _hover={{ color: 'white' }}>
+                React Flow Pro <ArrowForwardIcon fontSize={19} ml={2} />
+              </Button>
             </Box>
           </Flex>
         </Box>
       </Box>
-
-      {/* <Flex px={3} py={5} bg="primary" color="white">
-        Get the most out of React Flow with our new Pro Subscription
-      </Flex> */}
 
       <ExampleFlow
         title="Feature-rich"
@@ -90,32 +107,101 @@ export default function Home() {
         color="red"
       />
 
-      <Box bgColor="bgDark">
-        <Box maxWidth="container.md" mx="auto" px={3} py={100}>
-          <Heading
-            mb={5}
-            fontFamily="body"
-            fontWeight="black"
-            textAlign="center"
-            size="2xl"
-            color="white"
-          >
-            Getting Started
+      <Box bgColor="bgDark" px={3} py={24}>
+        <Box textAlign="center" m="0 auto" maxWidth="container.md">
+          <Heading mb={5} fontFamily="body" fontWeight="black" size="2xl" color="white">
+            Showcase
           </Heading>
-          <Heading
-            fontFamily="body"
-            fontWeight="bold"
-            size="md"
-            mx="auto"
-            textAlign="center"
-            color="gray.300"
-          >
-            React Flow enables you to build node-based applications. From simple static diagrams to
-            complex interactive editors. To get started, you can install it via npm.
+          <Heading mb={20} fontFamily="body" size="md" mx="auto" color="gray.300" fontWeight="bold">
+            React Flow can be used for a wide range of applications: Music Synthesizers, Machine
+            Learning Modeling, Conversational UIs.
           </Heading>
-          <Box mt={10}>
-            <CodeBlock language="bash">npm install react-flow-renderer</CodeBlock>
+        </Box>
+        <Flex justifyContent="space-between" maxWidth="container.xl" mx="auto">
+          <Box flex={1} width="48%">
+            <Box
+              backgroundImage="url(/img/datablocks.png)"
+              h={350}
+              backgroundSize="cover"
+              backgroundPosition="center"
+              borderRadius="md"
+            />
+            <Heading mb={5} mt={6} fontFamily="body" fontWeight="black" size="lg" color="white">
+              Datablocks
+            </Heading>
+            <Heading fontFamily="body" fontWeight="bold" size="md" mx="auto" color="gray.300">
+              A node-based editor for exploring, analyzing and transforming data without code. React
+              Flow was originally build for this project and later open sourced seperately.
+            </Heading>
           </Box>
+          <Box flex={1} width="48%">
+            <Flex flexWrap="wrap" justifyContent="space-between">
+              <Box mb={9} width="48%">
+                <Box
+                  backgroundImage="url(/img/datablocks.png)"
+                  h={180}
+                  backgroundSize="cover"
+                  backgroundPosition="center"
+                  borderRadius="md"
+                />
+                <Heading mb={3} mt={4} fontFamily="body" fontWeight="black" size="md" color="white">
+                  Datablocks
+                </Heading>
+              </Box>
+              <Box mb={9} width="48%">
+                <Box
+                  backgroundImage="url(/img/datablocks.png)"
+                  h={180}
+                  backgroundSize="cover"
+                  backgroundPosition="center"
+                  borderRadius="md"
+                />
+                <Heading mb={3} mt={4} fontFamily="body" fontWeight="black" size="md" color="white">
+                  Datablocks
+                </Heading>
+              </Box>
+              <Box mb={9} width="48%">
+                <Box
+                  backgroundImage="url(/img/datablocks.png)"
+                  h={180}
+                  backgroundSize="cover"
+                  backgroundPosition="center"
+                  borderRadius="md"
+                />
+                <Heading mb={3} mt={4} fontFamily="body" fontWeight="black" size="md" color="white">
+                  Datablocks
+                </Heading>
+              </Box>
+              <Box mb={9} width="48%">
+                <Flex
+                  border="1px dashed"
+                  borderColor="gray.700"
+                  h={180}
+                  backgroundSize="cover"
+                  backgroundPosition="center"
+                  borderRadius="md"
+                  color="gray.300"
+                  fontWeight="bold"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  your project here
+                </Flex>
+              </Box>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+
+      <Box px={3} py={24}>
+        <Box textAlign="center" m="0 auto" maxWidth="container.md">
+          <Heading mb={5} fontFamily="body" fontWeight="black" size="2xl">
+            Showcase
+          </Heading>
+          <Heading mb={20} fontFamily="body" size="md" mx="auto" color="gray.300" fontWeight="bold">
+            React Flow can be used for a wide range of applications: Music Synthesizers, Machine
+            Learning Modeling, Conversational UIs.
+          </Heading>
         </Box>
       </Box>
     </Layout>
