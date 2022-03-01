92e748dba7425a13bbabca4aada230cd523ac334
Christopher Möller <chrtze@gmail.com>
2022-03-01 13:18:41 +0100

    add typeform to clients

--- src/components/UsedBy/index.js
+++ src/components/UsedBy/index.js
@@ -5,30 +5,22 @@ import DoubleLoopLogo from '../../../static/img/used-by/doubleloop.svg';
 import LinkedInLogo from '../../../static/img/used-by/linkedin.svg';
 import BitDevLogo from '../../../static/img/used-by/bitdev.svg';
 import BasisLogo from '../../../static/img/used-by/basis.svg';
+import TypeformLogo from '../../../static/img/used-by/typeform.svg';
+
+const CompanyLogo = ({ Logo, ...props }) => (
+  <Box height="30px" {...props}>
+    <Logo height="100%" />
+  </Box>
+);
 
 export default function UsedBy() {
   return (
-    <SimpleGrid columns={[2, 4]} filter="contrast(0) brightness(1.4) saturate(0)">
-      <Tooltip label="Doubleloop uses React Flow for their editor.">
-        <Box height="30px">
-          <DoubleLoopLogo height="100%" />
-        </Box>
-      </Tooltip>
-      <Tooltip label="LinkedIn uses React Flow for an internal tool.">
-        <Box height="30px">
-          <LinkedInLogo height="100%" />
-        </Box>
-      </Tooltip>
-      <Tooltip label="bit.dev uses React Flow to visualize component dependencies.">
-        <Box height="30px">
-          <BitDevLogo height="100%" />
-        </Box>
-      </Tooltip>
-      <Tooltip label="getbasis.com uses React Flow for their interactive editor.">
-        <Box height="30px">
-          <BasisLogo height="100%" />
-        </Box>
-      </Tooltip>
+    <SimpleGrid columns={[2, 5]} filter="contrast(0) brightness(1.4) saturate(0)">
+      <CompanyLogo Logo={LinkedInLogo} />
+      <CompanyLogo Logo={DoubleLoopLogo} />
+      <CompanyLogo Logo={TypeformLogo} />
+      <CompanyLogo Logo={BitDevLogo} />
+      <CompanyLogo Logo={BasisLogo} />
     </SimpleGrid>
   );
 }
