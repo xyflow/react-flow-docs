1bc7012e4f6a068742741178c5f3636dcfd95c91
moklick <info@moritzklack.com>
2022-03-06 15:25:06 +0100

    chore(examples): add touch device

---
+++ src/components/CodeViewer/example-flows/TouchDevice/index.js
@@ -0,0 +1,43 @@
+import { useCallback } from 'react';
+import ReactFlow, { useNodesState, useEdgesState, Position, addEdge } from 'react-flow-renderer';
+
+import './index.css';
+
+const initialNodes = [
+  {
+    id: '1',
+    data: { label: 'Node 1' },
+    position: { x: 100, y: 100 },
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left,
+  },
+  {
+    id: '2',
+    data: { label: 'Node 2' },
+    position: { x: 300, y: 100 },
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left,
+  },
+];
+
+const initialEdges = [];
+
+const TouchDeviceFlow = () => {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback((connection) => setEdges((eds) => addEdge(connection, eds)), []);
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onConnect={onConnect}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      className="touchdevice-flow"
+      fitView
+    />
+  );
+};
+
+export default TouchDeviceFlow;
