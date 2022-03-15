e0d12c5f359b46baae3ff6c163377ad26e86ca7d
moklick <info@moritzklack.com>
2022-03-15 19:39:02 +0100

    chore(stats): add defaults

--- scripts/get-stats.js
+++ scripts/get-stats.js
@@ -6,6 +6,11 @@ const GITHUB_API_URL = 'https://api.github.com/repos/wbkd/react-flow';
 const NPM_API_URL = 'https://api.npmjs.org/downloads/point/last-year/react-flow-renderer';
 const OUTPUT_PATH = path.resolve(__dirname, '../static/data/stats.json');
 
+const defaultStats = {
+  stars: 9400,
+  downloads: 2890000,
+};
+
 const fetchJSON = async function (url) {
   return new Promise((resolve, reject) => {
     https.get(url, { headers: { 'User-Agent': 'webkid' } }, (response) => {
@@ -20,7 +25,10 @@ const fetchJSON = async function (url) {
 (async () => {
   const { stargazers_count: stars } = await fetchJSON(GITHUB_API_URL);
   const { downloads } = await fetchJSON(NPM_API_URL);
-  const stats = { stars, downloads };
+  const stats = {
+    stars: stars || defaultStats.stars,
+    downloads: downloads || defaultStats.downloads,
+  };
 
   fs.writeFileSync(OUTPUT_PATH, JSON.stringify(stats));
 })();
