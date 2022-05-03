8ab1bb1daf4f003297c30a3ea302c6f553b25aa7
moklick <info@moritzklack.com>
2022-05-03 23:35:26 +0200

    fix(example): use unique ids for edges closes #12

--- src/components/CodeViewer/api-flows/CustomNode2/index.js
+++ src/components/CodeViewer/api-flows/CustomNode2/index.js
@@ -29,7 +29,7 @@ const initialNodes = [
 
 const initialEdges = [
   { id: 'edge-1', source: 'node-1', target: 'node-2', sourceHandle: 'a' },
-  { id: 'edge-1', source: 'node-1', target: 'node-3', sourceHandle: 'b' },
+  { id: 'edge-2', source: 'node-1', target: 'node-3', sourceHandle: 'b' },
 ];
 
 // we define the nodeTypes outside of the component to prevent re-renderings
