cc913a7216524f60911b79f29d71138d5c53a383
Christopher Möller <chrtze@gmail.com>
2022-08-08 18:26:49 +0200

    refactor(showcase): reduce image size

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,8 +21,8 @@ const config = {
       '@docusaurus/plugin-ideal-image',
       {
         quality: 70,
-        max: 1030, // max resized image's size.
-        min: 640, // min resized image's size. if original is lower, use that size.
+        max: 800, // max resized image's size.
+        min: 400, // min resized image's size. if original is lower, use that size.
         steps: 2, // the max number of images generated between min and max (inclusive)
         disableInDev: false,
       },
