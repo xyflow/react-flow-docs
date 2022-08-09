bbd92a2db4bb34a6e6366197a3c88064547b98ee
moklick <info@moritzklack.com>
2022-08-09 22:49:45 +0200

    chore(docitem): cleanup

--- src/theme/DocItem/Metadata/index.js
+++ src/theme/DocItem/Metadata/index.js
@@ -3,6 +3,10 @@ import { PageMetadata } from '@docusaurus/theme-common';
 import { useDoc } from '@docusaurus/theme-common/internal';
 
 function getPageTitle(metadata) {
+  if (!metadata.title) {
+    return '';
+  }
+
   let appendix = '';
 
   if (metadata.source?.includes('/api/')) {
@@ -15,11 +19,7 @@ function getPageTitle(metadata) {
     appendix = ' Docs';
   }
 
-  if (metadata.title) {
-    return `${metadata.title}${appendix}`;
-  }
-
-  return 'React Flow';
+  return `${metadata.title}${appendix}`;
 }
 
 export default function DocItemMetadata() {
