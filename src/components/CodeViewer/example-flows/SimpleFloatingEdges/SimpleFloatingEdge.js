b0a1dbb493510549f772b1f8e67a711cae2f0ecb
moklick <info@moritzklack.com>
2022-08-25 15:09:11 +0200

    feat(examples): add simple edge example

---
+++ src/components/CodeViewer/example-flows/SimpleFloatingEdges/SimpleFloatingEdge.js
@@ -0,0 +1,32 @@
+import { useCallback } from 'react';
+import { useStore, getBezierPath } from 'react-flow-renderer';
+
+import { getEdgeParams } from './utils.js';
+
+function SimpleFloatingEdge({ id, source, target, markerEnd, style }) {
+  const sourceNode = useStore(useCallback((store) => store.nodeInternals.get(source), [source]));
+  const targetNode = useStore(useCallback((store) => store.nodeInternals.get(target), [target]));
+
+  if (!sourceNode || !targetNode) {
+    return null;
+  }
+
+  const { sx, sy, tx, ty, sourcePos, targetPos } = getEdgeParams(sourceNode, targetNode);
+
+  const d = getBezierPath({
+    sourceX: sx,
+    sourceY: sy,
+    sourcePosition: sourcePos,
+    targetPosition: targetPos,
+    targetX: tx,
+    targetY: ty,
+  });
+
+  return (
+    <g className="react-flow__connection">
+      <path id={id} className="react-flow__edge-path" d={d} markerEnd={markerEnd} style={style} />
+    </g>
+  );
+}
+
+export default SimpleFloatingEdge;
---
+++ src/components/CodeViewer/example-flows/SimpleFloatingEdges/index.js
@@ -0,0 +1,79 @@
+import React, { useCallback } from 'react';
+import ReactFlow, {
+  addEdge,
+  Background,
+  useNodesState,
+  useEdgesState,
+  MarkerType,
+  ConnectionMode,
+} from 'react-flow-renderer';
+
+import SimpleFloatingEdge from './SimpleFloatingEdge';
+import CustomNode from './CustomNode';
+
+import './index.css';
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const edgeTypes = {
+  floating: SimpleFloatingEdge,
+};
+
+const initialNodes = [
+  {
+    id: '1',
+    label: '1',
+    position: { x: 0, y: 0 },
+    data: { label: 'drag me around 😎' },
+    type: 'custom',
+  },
+  {
+    id: '2',
+    label: '2',
+    position: { x: 0, y: 150 },
+    data: { label: '...or me' },
+    type: 'custom',
+  },
+];
+
+const initialEdges = [
+  { source: '1', target: '2', type: 'floating', markerEnd: { type: MarkerType.ArrowClosed } },
+];
+
+const fitViewOptions = { padding: 4 };
+
+const NodeAsHandleFlow = () => {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params) =>
+      setEdges((eds) =>
+        addEdge({ ...params, type: 'floating', markerEnd: { type: MarkerType.Arrow } }, eds)
+      ),
+    []
+  );
+
+  return (
+    <div className="simple-floatingedges">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        edgeTypes={edgeTypes}
+        nodeTypes={nodeTypes}
+        fitView
+        fitViewOptions={fitViewOptions}
+        connectionMode={ConnectionMode.Loose}
+      >
+        <Background />
+      </ReactFlow>
+    </div>
+  );
+};
+
+export default NodeAsHandleFlow;
