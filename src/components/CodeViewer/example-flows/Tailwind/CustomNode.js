c542b5fb0bb6e20dcc01fce6593aa4967d8dfc19
moklick <info@moritzklack.com>
2022-10-18 14:37:01 +0200

    chore(tailwind): cleanup

--- src/components/CodeViewer/example-flows/Tailwind/CustomNode.js
+++ src/components/CodeViewer/example-flows/Tailwind/CustomNode.js
@@ -3,7 +3,7 @@ import { Handle, Position } from 'reactflow';
 
 function CustomNode({ data }) {
   return (
-    <div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400	">
+    <div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400">
       <div className="flex">
         <div className="rounded-full w-12 h-12 flex justify-center items-center bg-gray-100">
           {data.emoji}
