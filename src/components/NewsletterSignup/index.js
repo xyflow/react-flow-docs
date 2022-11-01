c76c056a2ff64a5b8950e7b8db8de89a1e4a7c15
johnrobbjr <johnhrobb@gmail.com>
2022-11-01 09:59:48 +0100

    chore(blog): add fall newsletter text and images

--- src/components/NewsletterSignup/index.js
+++ src/components/NewsletterSignup/index.js
@@ -55,7 +55,7 @@ export default function NewsletterSignup({ size = 'md', ...props }) {
               required
               bg="white"
               type="email"
-              placeholder="you@email.com"
+              placeholder="Your Mail"
               size={size}
               borderRadius="lg"
               name="email"
