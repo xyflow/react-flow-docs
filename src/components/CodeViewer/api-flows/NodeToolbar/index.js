e6426e283396ae84c5c3f437f144fa8910f2f80e
Christopher Möller <chrtze@gmail.com>
2022-12-06 20:05:53 +0100

    feat(node-toolbar): update node toolbar example

--- src/components/CodeViewer/api-flows/NodeToolbar/index.js
+++ src/components/CodeViewer/api-flows/NodeToolbar/index.js
@@ -17,59 +17,58 @@ const nodeTypes = {
   tooltip: TooltipNode,
 };
 
+const defaultNodeStyle = {
+  border: '2px solid #ff0071',
+  background: 'white',
+  borderRadius: 20,
+};
+
 const initialNodes = [
   {
     id: '1',
     type: 'custom',
     data: { label: 'toolbar top', toolbarPosition: Position.Top },
-    position: { x: 0, y: 50 },
-    className: 'react-flow__node-default',
+    position: { x: 200, y: 0 },
+    style: defaultNodeStyle,
   },
   {
     id: '2',
     type: 'custom',
     data: { label: 'toolbar right', toolbarPosition: Position.Right },
-    position: { x: 300, y: 0 },
-    className: 'react-flow__node-default',
+    position: { x: -50, y: 100 },
+    style: defaultNodeStyle,
   },
   {
     id: '3',
     type: 'custom',
     data: { label: 'toolbar bottom', toolbarPosition: Position.Bottom },
-    position: { x: 400, y: 100 },
-    className: 'react-flow__node-default',
+    position: { x: 0, y: 200 },
+    style: defaultNodeStyle,
   },
   {
     id: '4',
     type: 'custom',
     data: { label: 'toolbar left', toolbarPosition: Position.Left },
-    position: { x: 400, y: 200 },
-    className: 'react-flow__node-default',
+    position: { x: 200, y: 300 },
+    style: defaultNodeStyle,
   },
   {
     id: '5',
     type: 'tooltip',
     data: { label: 'hover tooltip', toolbarPosition: Position.Left },
-    position: { x: 300, y: 400 },
-    className: 'react-flow__node-default',
+    position: { x: 100, y: 400 },
+    style: defaultNodeStyle,
   },
   {
     id: '6',
     type: 'custom',
     data: { label: 'toolbar always open', toolbarPosition: Position.Top, toolbarVisible: true },
-    position: { x: 0, y: 200 },
-    className: 'react-flow__node-default',
+    position: { x: 0, y: -100 },
+    style: defaultNodeStyle,
   },
 ];
 
-const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2' },
-  { id: 'e1-3', source: '1', target: '3' },
-  { id: 'e1-4', source: '1', target: '4' },
-];
-
-const defaultEdgeOptions = { zIndex: 0 };
-const nodeOrigin = [0.5, 0.5];
+const initialEdges = [];
 
 export default function NodeToolbarExample() {
   return (
@@ -80,9 +79,8 @@ export default function NodeToolbarExample() {
       minZoom={0.2}
       maxZoom={4}
       fitView
-      defaultEdgeOptions={defaultEdgeOptions}
+      fitViewOptions={{ padding: 0.5 }}
       nodeTypes={nodeTypes}
-      nodeOrigin={nodeOrigin}
     >
       <Background variant={BackgroundVariant.Dots} />
       <MiniMap />
