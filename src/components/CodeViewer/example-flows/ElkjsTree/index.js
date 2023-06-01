e93d5895c92c0ae2f33ebb6cc564f2a2b59a15e8
Hayleigh Thompson <me@hayleigh.dev>
2023-06-01 09:28:26 +0100

    :recycle: Tweak example to more closely match the dagre example wrt organisation.

--- src/components/CodeViewer/example-flows/ElkjsTree/index.js
+++ src/components/CodeViewer/example-flows/ElkjsTree/index.js
@@ -1,86 +1,111 @@
 import { initialNodes, initialEdges } from './nodes-edges.js';
 import ELK from 'elkjs/lib/elk.bundled.js';
-import React, { useCallback, useState, useEffect } from 'react';
-import ReactFlow, { addEdge, Panel, useNodesState, useEdgesState } from 'reactflow';
+import React, { useCallback, useLayoutEffect } from 'react';
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  Panel,
+  useNodesState,
+  useEdgesState,
+  useReactFlow,
+} from 'reactflow';
 
 import 'reactflow/dist/style.css';
 
 const elk = new ELK();
 
-const useLayoutedElements = (nodes, edges, options = {}) => {
-  const [layoutedNodes, setLayoutedNodes] = useNodesState([]);
-  const [layoutedEdges, setLayoutedEdges] = useEdgesState([]);
-
-  useEffect(() => {
-    const isHorizontal = options?.['elk.direction'] === 'RIGHT';
-    const graph = {
-      id: 'root',
-      layoutOptions: options,
-      children: nodes.map((node) => ({
-        ...node,
-        // Adjust the target and source handle positions based on the layout
-        // direction.
-        targetPosition: isHorizontal ? 'left' : 'top',
-        sourcePosition: isHorizontal ? 'right' : 'bottom',
-
-        // Hardcode a width and height for elk to use when layouting.
-        width: 150,
-        height: 50,
-      })),
-      edges: edges,
-    };
-
-    elk
-      .layout(graph)
-      .then((layoutedGraph) => {
-        setLayoutedNodes(
-          layoutedGraph.children.map((node) => ({
-            ...node,
-            // React Flow expects a position property on the node instead of `x`
-            // and `y` fields.
-            position: { x: node.x, y: node.y },
-          }))
-        );
-        setLayoutedEdges(layoutedGraph.edges);
-      })
-      .catch(console.error);
-  }, [nodes, edges, options]);
-
-  return { nodes: layoutedNodes, edges: layoutedEdges };
+// Elk has a *huge* amount of options to configure. To see everything you can
+// tweak check out:
+//
+// - https://www.eclipse.org/elk/reference/algorithms.html
+// - https://www.eclipse.org/elk/reference/options.html
+const elkOptions = {
+  'elk.algorithm': 'layered',
+  'elk.layered.spacing.nodeNodeBetweenLayers': '100',
+  'elk.spacing.nodeNode': '80',
 };
 
-export default function LayoutFlow() {
-  const [nodes, _, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const [options, setOptions] = useState({
-    'elk.algorithm': 'layered',
-    'elk.direction': 'DOWN',
-    'elk.layered.spacing.nodeNodeBetweenLayers': '100',
-    'elk.spacing.nodeNode': '80',
-  });
+const getLayoutedElements = (nodes, edges, options = {}) => {
+  const isHorizontal = options?.['elk.direction'] === 'RIGHT';
+  const graph = {
+    id: 'root',
+    layoutOptions: options,
+    children: nodes.map((node) => ({
+      ...node,
+      // Adjust the target and source handle positions based on the layout
+      // direction.
+      targetPosition: isHorizontal ? 'left' : 'top',
+      sourcePosition: isHorizontal ? 'right' : 'bottom',
 
-  const { nodes: layoutedNodes, edges: layoutedEdges } = useLayoutedElements(nodes, edges, options);
+      // Hardcode a width and height for elk to use when layouting.
+      width: 150,
+      height: 50,
+    })),
+    edges: edges,
+  };
+
+  return elk
+    .layout(graph)
+    .then((layoutedGraph) => ({
+      nodes: layoutedGraph.children.map((node) => ({
+        ...node,
+        // React Flow expects a position property on the node instead of `x`
+        // and `y` fields.
+        position: { x: node.x, y: node.y },
+      })),
+
+      edges: layoutedGraph.edges,
+    }))
+    .catch(console.error);
+};
+
+function LayoutFlow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const { fitView } = useReactFlow();
 
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+  const onLayout = useCallback(
+    ({ direction, useInitialNodes = false }) => {
+      const opts = { 'elk.direction': direction, ...elkOptions };
+      const ns = useInitialNodes ? initialNodes : nodes;
+      const es = useInitialNodes ? initialEdges : edges;
+
+      getLayoutedElements(ns, es, opts).then(({ nodes: layoutedNodes, edges: layoutedEdges }) => {
+        setNodes(layoutedNodes);
+        setEdges(layoutedEdges);
+
+        window.requestAnimationFrame(() => fitView());
+      });
+    },
+    [nodes, edges]
+  );
+
+  // Calculate the initial layout on mount.
+  useLayoutEffect(() => {
+    onLayout({ direction: 'DOWN', useInitialNodes: true });
+  }, []);
 
   return (
     <ReactFlow
-      nodes={layoutedNodes}
-      edges={layoutedEdges}
+      nodes={nodes}
+      edges={edges}
       onConnect={onConnect}
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
       fitView
     >
       <Panel position="top-right">
-        <button onClick={() => setOptions((options) => ({ ...options, 'elk.direction': 'DOWN' }))}>
-          vertical layout
-        </button>
+        <button onClick={() => onLayout({ direction: 'DOWN' })}>vertical layout</button>
 
-        <button onClick={() => setOptions((options) => ({ ...options, 'elk.direction': 'RIGHT' }))}>
-          horizontal layout
-        </button>
+        <button onClick={() => onLayout({ direction: 'RIGHT' })}>horizontal layout</button>
       </Panel>
     </ReactFlow>
   );
 }
+
+export default () => (
+  <ReactFlowProvider>
+    <LayoutFlow />
+  </ReactFlowProvider>
+);
