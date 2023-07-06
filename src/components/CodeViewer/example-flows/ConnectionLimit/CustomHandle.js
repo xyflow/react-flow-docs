1e4d1ed00aa2db0ed06aa6bf7ef99eb52d110c8d
Hayleigh Thompson <me@hayleigh.dev>
2023-07-06 11:57:49 +0100

    :recycle: Refactor custom handle to avoid common gotchas with accessing 'nodeInteranals'.

--- src/components/CodeViewer/example-flows/ConnectionLimit/CustomHandle.js
+++ src/components/CodeViewer/example-flows/ConnectionLimit/CustomHandle.js
@@ -1,28 +1,33 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { getConnectedEdges, Handle, useNodeId, useStore } from 'reactflow';
 
-const selector = (s) => ({
-  nodeInternals: s.nodeInternals,
-  edges: s.edges,
-});
+const selector =
+  (nodeId, isConnectable = true, maxConnections = Infinity) =>
+  (s) => {
+    // If the user props say this handle is not connectable, we don't need to
+    // bother checking anything else.
+    if (!isConnectable) return false;
+
+    const node = s.nodeInternals.get(nodeId);
+    const connectedEdges = getConnectedEdges([node], s.edges);
+
+    return connectedEdges.length < maxConnections;
+  };
 
 const CustomHandle = (props) => {
-  const { nodeInternals, edges } = useStore(selector);
   const nodeId = useNodeId();
+  const isConnectable = useStore(
+    useCallback(selector(nodeId, props.isConnectable, props.maxConnections), [
+      nodeId,
+      props.isConnectable,
+      props.maxConnections,
+    ])
+  );
 
-  const isConnectable = useMemo(() => {
-    const node = nodeInternals.get(nodeId);
-    const connectedEdges = getConnectedEdges([node], edges);
-    const maxConnections = props.maxConnections ?? Infinity;
-
-    // The `isConnectable` prop can be passed to any Handle to enable/disable
-    // it: it's part of React Flow!
-    //
-    // https://reactflow.dev/docs/api/nodes/handle/#isconnectable
-    return props.isConnectable && connectedEdges.length < maxConnections;
-  }, [nodeInternals, edges, nodeId, props.isConnectable, props.maxConnections]);
-
-  return <Handle {...props} type="target" isConnectable={isConnectable}></Handle>;
+  // The `isConnectable` prop is a part of React Flow, all we need to do is give
+  // it the bool we calculated above and React Flow can handle the logic to disable
+  // it for us.
+  return <Handle {...props} type="target" isConnectable={isConnectable} />;
 };
 
 export default CustomHandle;
